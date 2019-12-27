63ac582e09f2eae55aff5a655d24004bc51e6765
“Rafael <fc52043@alunos.fc.ul.pt>
2019-12-27 16:15:29 +0000

    Adding jdbc authentication

--- src/main/java/tacos/repositories/OrderRepository.java
+++ src/main/java/tacos/repositories/OrderRepository.java
@@ -3,7 +3,6 @@ package tacos.repositories;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import tacos.entities.Order;
@@ -13,9 +12,6 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
 	List<Order> findByDeliveryZip(String deliveryZip);
 	
 	List<Order> readByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
-	
-	@Query("Order o where o.deliveryCity='Seattle'")
-	List<Order> readOrdersDeliveredInSeattle();
 	
 	
 }
