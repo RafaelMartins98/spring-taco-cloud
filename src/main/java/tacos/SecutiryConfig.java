56c11435597946750389f6011065cd7f6cb53ebe
“Rafael <fc52043@alunos.fc.ul.pt>
2019-12-29 14:20:30 +0000

    Adding ldap authentication example

--- src/main/java/tacos/SecutiryConfig.java
+++ src/main/java/tacos/SecutiryConfig.java
@@ -3,6 +3,7 @@ package tacos;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,6 +11,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecutiryConfig extends WebSecurityConfigurerAdapter{
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		
+		// Without passwordCompare(), it will try to authenticate the user on
+		// LDAP server, otherwise with this method, it will only compare passwords
+		
+		
+		// LDAP Authentication port -> 33389
+		
+		auth.ldapAuthentication()
+			.userSearchBase("ou=people")
+			.userSearchFilter("(uid={0})")
+			.contextSource()
+			.root("dc=tacocloud,dc=com")
+			.ldif("classpath:users.ldif").and()
+			.passwordCompare()
+			.passwordAttribute("userPassword");
+
+	}
 	
 
 }
