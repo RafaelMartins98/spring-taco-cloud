59fefe274984c564ab9c0f1c726eed6a7f8da1a2
“Rafael <fc52043@alunos.fc.ul.pt>
2019-12-29 21:45:25 +0000

    Adding user entities

--- src/main/java/tacos/SecutiryConfig.java
+++ src/main/java/tacos/SecutiryConfig.java
@@ -2,19 +2,43 @@ package tacos;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecutiryConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private UserDetailsService userDetailsService; 
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(encoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.antMatchers("/design", "/orders")
+			.access("hasRole('ROLE_USER')")
+			.antMatchers("/", "/**").permitAll();
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 
---
+++ src/main/java/tacos/services/UserRepositoryUserDetaulsService.java
@@ -0,0 +1,34 @@
+package tacos.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import tacos.entities.User;
+import tacos.repositories.UserRepository;
+
+@Service
+public class UserRepositoryUserDetaulsService implements UserDetailsService {
+
+	private UserRepository userRepository;
+
+	@Autowired
+	public UserRepositoryUserDetaulsService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = userRepository.findByUsername(username);
+
+		if (user != null) {
+			return user;
+		}
+
+		throw new UsernameNotFoundException("User called by " + username + "doesn't exist");
+
+	}
+
+}
