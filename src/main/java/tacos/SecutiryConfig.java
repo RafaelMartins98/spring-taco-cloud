53cfc2746f9ab7ba0a92dbf926fb3ff4905a6ac1
“Rafael <fc52043@alunos.fc.ul.pt>
2019-12-29 14:22:12 +0000

    Adding initial structure

--- src/main/java/tacos/SecutiryConfig.java
+++ src/main/java/tacos/SecutiryConfig.java
@@ -14,21 +14,6 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		
-		// Without passwordCompare(), it will try to authenticate the user on
-		// LDAP server, otherwise with this method, it will only compare passwords
-		
-		
-		// LDAP Authentication port -> 33389
-		
-		auth.ldapAuthentication()
-			.userSearchBase("ou=people")
-			.userSearchFilter("(uid={0})")
-			.contextSource()
-			.root("dc=tacocloud,dc=com")
-			.ldif("classpath:users.ldif").and()
-			.passwordCompare()
-			.passwordAttribute("userPassword");
 
 	}
 	
