6a4d7dea8eb3e8844a37f59d178b5b6a4e9c078e
“Rafael <fc52043@alunos.fc.ul.pt>
2020-01-05 17:20:57 +0000

    Adding renamed resource

--- tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
+++ tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -1,16 +1,8 @@
 package tacos.web.api;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.hateoas.EntityLinks;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,12 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import tacos.Taco;
 import tacos.data.TacoRepository;
 
-@RestController
 @RequestMapping(path = "/design", produces = "application/json")
 @CrossOrigin(origins = "*")
 public class DesignTacoController {
@@ -37,21 +27,6 @@ public class DesignTacoController {
 		this.tacoRepository = tacoRepository;
 	}
 
-	@GetMapping("/recent")
-	public Resources<TacoResource> recentTacos() {
-
-		Page<Taco> tacos = tacoRepository.findAll(PageRequest.of(0, 12, Sort.by("createdAt").descending()));
-
-		List<TacoResource> tacoResources = TacoResource.tacoAssembler.toResources(tacos.getContent());
-		
-		Resources<TacoResource> resources = new Resources<>(tacoResources);
-		
-		
-		resources.add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(DesignTacoController.class).recentTacos())
-										   .withRel("recents"));
-
-		return resources;
-	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
